57a8fe0edaabcd904ef8f6830d4a47409a8ece4b
Christopher Snyder <snyderchris30@gmail.com>
2026-04-11 16:30:00 -0500

    Fix: move inline styles to CSS, correct ARIA attribute value

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
 
 
+import "./not-found.css";
+
 export default function NotFound() {
   return (
-    <div style={{ color: 'red', padding: 24 }}>
+    <div className="not-found-container">
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
